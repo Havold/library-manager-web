fec7a1191b043307bda3bf1f5a25606488b36b78
Havold <21521479@gm.uit.edu.vn>
2024-08-18 18:06:44 +0700

    Create UI Add New Product and User.

---
+++ src/formSource.js
@@ -0,0 +1,89 @@
+export const userInputs = [
+    {
+        id: 1,
+        label: 'Username',
+        type: 'text',
+        placeholder: 'nguyenthanhtung123',
+    },
+    {
+        id: 2,
+        label: 'Name and surname',
+        type: 'text',
+        placeholder: 'Nguyen Thanh Tung',
+    },
+    {
+        id: 3,
+        label: 'Email',
+        type: 'email',
+        placeholder: 'sontungmtp@gmail.com"',
+    },
+    {
+        id: 4,
+        label: 'Phone',
+        type: 'text',
+        placeholder: '0918234213',
+    },
+    {
+        id: 5,
+        label: 'Password',
+        type: 'password',
+        placeholder: '',
+    },
+    {
+        id: 6,
+        label: 'Address',
+        type: 'text',
+        placeholder: 'Thai Binh City, Thai Binh Province',
+    },
+    {
+        id: 7,
+        label: 'Country',
+        type: 'text',
+        placeholder: 'Vietnam',
+    },
+];
+
+export const bookInputs = [
+    {
+        id: 1,
+        label: 'Title',
+        type: 'text',
+        placeholder: 'Harry Porter',
+    },
+    {
+        id: 2,
+        label: 'Author',
+        type: 'text',
+        placeholder: 'J. K. Rowling',
+    },
+    {
+        id: 3,
+        label: 'Language',
+        type: 'text',
+        placeholder: 'English',
+    },
+    {
+        id: 4,
+        label: 'Published year',
+        type: 'text',
+        placeholder: '2003',
+    },
+    {
+        id: 5,
+        label: 'Category',
+        type: 'text',
+        placeholder: 'Fantasy, Comedy"',
+    },
+    {
+        id: 6,
+        label: 'Description',
+        type: 'text',
+        placeholder: 'Description',
+    },
+    {
+        id: 7,
+        label: 'Stock',
+        type: 'text',
+        placeholder: '20',
+    },
+];
--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,7 +1,76 @@
+import { ArrowBack, DriveFolderUploadOutlined } from '@mui/icons-material';
+import Navbar from '../../components/navbar/Navbar';
+import Sidebar from '../../components/sidebar/Sidebar';
 import './new.scss';
+import { useEffect, useState } from 'react';
 
-function New() {
-    return <div className="new">New page</div>;
+const defaultImg = 'https://i.pinimg.com/564x/37/08/99/3708994bdca38cd8dbea509f233f3cf4.jpg';
+
+function New({ inputs, title }) {
+    const [img, setImg] = useState({ preview: defaultImg });
+
+    const handleChangeImg = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            file.preview = URL.createObjectURL(file);
+            setImg(file);
+            e.target.value = null;
+            console.log(img);
+        }
+    };
+
+    useEffect(() => {
+        return () => {
+            if (img) {
+                URL.revokeObjectURL(img.preview);
+                console.log('Cleanup');
+            }
+        };
+    }, [img]);
+
+    return (
+        <div className="new">
+            <Sidebar />
+            <div className="newContainer">
+                <Navbar />
+                <div className="top">
+                    <ArrowBack className="icon" />
+                    <h1>{title}</h1>
+                </div>
+
+                <div className="bottom">
+                    <div className="left">
+                        <img src={img.preview} alt="no-image" />
+                    </div>
+                    <div className="right">
+                        <form>
+                            <div className="formInput">
+                                <label htmlFor="uploadImg">
+                                    Image: <DriveFolderUploadOutlined className="icon" />
+                                </label>
+                                <input
+                                    type="file"
+                                    id="uploadImg"
+                                    onChange={handleChangeImg}
+                                    style={{ display: 'none' }}
+                                />
+                            </div>
+
+                            {inputs.map((item) => {
+                                return (
+                                    <div className="formInput">
+                                        <label>{item.label}</label>
+                                        <input type={item.type} placeholder={item.placeholder} />
+                                    </div>
+                                );
+                            })}
+                        </form>
+                        <button>Add</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default New;
